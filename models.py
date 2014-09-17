f324ec20972e5b0ac1682a66362a998d4182277e
EndlessCheng <loli.con@qq.com>
2014-09-17 21:07:58 +0800

    添加并基本完成借阅者和管理者界面

--- models.py
+++ models.py
@@ -3,16 +3,18 @@ from django.db import models
 
 # just def the member variable in each class
 class User(models.Model):
-	account = models.EmailField(max_length = 50, primary_key = True)
-	name = models.CharField(max_length = 40)
+	account = models.CharField(max_length = 20, primary_key = True)
+	name = models.CharField(max_length = 20)
 	passwd = models.CharField(max_length = 20)
+	email = models.EmailField(max_length = 50)
 	
 class Book(models.Model):
 	isbn = models.IntegerField(max_length = 13, primary_key = True)
 	bookname = models.CharField(max_length = 50)
+	number = models.IntegerField()
 	authorname = models.CharField(max_length = 50)
 	booktype = models.CharField(max_length = 50)
 	callnumber = models.CharField(max_length = 20)
 	publisher = models.CharField(max_length = 50)
 	puclishtime = models.DateField()
-	price = models.DecimalField(max_digits = 10, decimal_palces = 2)
+	price = models.DecimalField(max_digits = 10, decimal_places = 2)
