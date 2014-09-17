b961afbcd53e11a4398dbddd0b2910d39a1ddf9b
EndlessCheng <loli.con@qq.com>
2014-09-17 21:08:34 +0800

    添加并基本完成借阅者和管理者界面

--- views.py
+++ views.py
@@ -12,12 +12,12 @@ def insert(request):
 		post = request.POST
 		new_people = People(name = post['name'], sex = (post['sex'] == 'M'), email = post['email'])
 		new_people.save()
-	return render_to_response('insert.html')
+	return render_to_response('template/insert.html')
 	
 def list(request):
 	people_list = People.objects.all()
 	c = Context({'people_list' : people_list})
-	return render_to_response('list.html', c)
+	return render_to_response('template/list.html', c)
 	
 def login(request):
 	ismatch = False
@@ -27,7 +27,7 @@ def login(request):
 		if len(user) == 1:
 			ismatch = True
 	c = Context({'ismatch': ismatch, 'email': request.POST.get('email', '-')})
-	return render_to_response('login.html', c)
+	return render_to_response('template/login.html', c)
 	
 def register(request):
 	exist = False
@@ -43,12 +43,21 @@ def register(request):
 				new_user = User(email = post['email'], name = post['name'], passwd = post['passwd'], )
 				new_user.save()
 	c = Context({'exist' : exist, 'same' : same, })
-	return render_to_response('register.html', c)
+	return render_to_response('template/register.html', c)
 
 def borrow(request):
 	if request.POST:
 		post = request.POST
 		book_list = Book.objects.get(bookname = post['bookname'])		
 	c = Context({'book_list' : book_list, })
-	return render_to_response('borrow.html', c)
+	return render_to_response('template/borrow.html', c)
+
+def show_userinfo(request):
+	pass
+
+def manage(request):
+	pass
+	
+	
+
 	
